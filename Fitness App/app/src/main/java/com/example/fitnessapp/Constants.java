ba977a6f6d9e3af1943b57dc3919d16c7571803e
abdulla875 <abdullahussan193@gmail.com>
2021-07-01 16:05:36 +0100

    Created the Activity

---
+++ Fitness App/app/src/main/java/com/example/fitnessapp/Constants.java
@@ -0,0 +1,20 @@
+package com.example.fitnessapp;
+
+import android.graphics.BitmapFactory;
+
+import java.util.ArrayList;
+
+public class Constants {
+    ArrayList<ExerciseData> listOfExerciseData = new ArrayList<>();
+    ExerciseData exerciseData = new ExerciseData();
+
+    public void getExercise(){
+        exerciseData.id = 1;
+        exerciseData.exercise = "push ups";
+
+    }
+
+
+
+
+}
---
+++ Fitness App/app/src/main/java/com/example/fitnessapp/ExerciseData.java
@@ -0,0 +1,9 @@
+package com.example.fitnessapp;
+
+import android.graphics.Bitmap;
+
+public class ExerciseData {
+    int id;
+    String exercise;
+    Bitmap image;
+}
